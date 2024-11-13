03de0fb6e3e8d88be52b9254bb7ad70c4307e814
egemencakir39 <115990783+egemencakir39@users.noreply.github.com>
2024-11-13 21:14:03 +0300

    Update ballController.cs

--- multiplayerDeneme/Assets/Scripts/ballController.cs
+++ multiplayerDeneme/Assets/Scripts/ballController.cs
@@ -2,61 +2,34 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(NetworkIdentity))]
 public class ballController : NetworkBehaviour
 {
     private Rigidbody2D rb;
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    [ServerCallback]
+    void FixedUpdate()
     {
-        // Eðer client authority'ye sahipsek, topu sadece client'ta hareket ettiriyoruz
-        if (isOwned)
+        // Topun sunucu tarafýndan hareket ettirilmesi ve fiziksel etkileþimlerinin kontrol edilmesi
+    }
+
+    [ServerCallback]
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
-            HandleBallMovement();
+            // Çarpma olayýný yönet
+            Vector2 force = collision.relativeVelocity * rb.mass;
+            rb.AddForce(force, ForceMode2D.Impulse);
         }
-    }
-
-    private void HandleBallMovement()
-    {
-        // Basit bir kontrol mekanizmasý; topu ileriye doðru hareket ettiriyoruz
-        float moveSpeed = 10f;
-        rb.velocity = new Vector2(moveSpeed, rb.velocity.y); // Yalnýzca X ekseninde hareket ettiriyoruz
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Sadece sunucu tarafýnda çalýþtýrmak için kontrol ediyoruz
-        if (isServer && collision.gameObject.CompareTag("Player"))
-        {
-            NetworkIdentity playerIdentity = collision.gameObject.GetComponent<NetworkIdentity>();
-            if (playerIdentity != null)
-            {
-                // Topun authority'si var mý? Varsa, önce authority'yi kaldýrýyoruz
-                NetworkIdentity ballIdentity = GetComponent<NetworkIdentity>();
-
-                if (ballIdentity.isOwned)
-                {
-                    ballIdentity.RemoveClientAuthority();
-                }
-
-                // Yetki devri iþlemi
-                AssignAuthority(playerIdentity.connectionToClient);
-            }
-        }
-    }
-
-    [Server]
-    private void AssignAuthority(NetworkConnectionToClient conn)
-    {
-        NetworkIdentity ballIdentity = GetComponent<NetworkIdentity>();
-
-        // Yeni client'a yetki veriyoruz
-        ballIdentity.AssignClientAuthority(conn);
     }
 }
